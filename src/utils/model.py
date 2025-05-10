09a985c31519f93327dcf5f81bc4e50444d392c4
Josua <jehret6@stud.hs-offenburg.de>
2025-05-10 23:07:03 +0200

    fix key

--- src/utils/model.py
+++ src/utils/model.py
@@ -13,7 +13,7 @@ def response(input, prompt=None, model="openai/o3-mini"):
     
     client = OpenAI(
         base_url="https://openrouter.ai/api/v1",    
-        api_key="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" 
+        api_key="API_KEY" 
     )
 
     combined_input = f"{prompt}\n\n---\n\n{input}"
