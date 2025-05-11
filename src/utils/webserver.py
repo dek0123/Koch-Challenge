4e88aebb881de87ec8ef2dcc508733c9afdee3e4
dio-mb <dionysm@gmail.com>
2025-05-11 11:08:21 +0200

    Change webserver to be more reliable on different OS (Windows handles paths differently)

--- src/utils/webserver.py
+++ src/utils/webserver.py
@@ -4,12 +4,21 @@ import socketserver
 import webbrowser
 import threading
 import os
+import sys
 
 # Configure port
 PORT = 8000
 
-# Move to the parent directory to serve files correctly
-os.chdir('..')
+# Get the absolute path to the parent directory
+script_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(script_dir)
+
+# Change to the parent directory
+os.chdir(parent_dir)
+
+# Define the path to the HTML file
+html_path = os.path.join('gui', 'xml-editor.html')
+url_path = f'http://localhost:{PORT}/{html_path.replace(os.sep, "/")}'
 
 # Define handler
 Handler = http.server.SimpleHTTPRequestHandler
@@ -19,15 +28,22 @@ httpd = socketserver.TCPServer(("", PORT), Handler)
 
 # Function to open browser pointing to the specific HTML file
 def open_browser():
-    webbrowser.open(f'http://localhost:{PORT}/gui/xml-editor.html')
+    print(f"Opening browser to {url_path}")
+    webbrowser.open(url_path)
 
 # Print server info
 print(f"Server started at http://localhost:{PORT}")
-print(f"Opening browser to http://localhost:{PORT}/gui/xml-editor.html")
+print(f"Current working directory: {os.getcwd()}")
+print(f"Target file path: {os.path.join(os.getcwd(), html_path)}")
 print("Press Ctrl+C to stop the server")
 
 # Open browser after a short delay (to ensure server is running)
 threading.Timer(0.5, open_browser).start()
 
 # Start server
-httpd.serve_forever()
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    print("\nShutting down server...")
+    httpd.server_close()
+    sys.exit(0)
