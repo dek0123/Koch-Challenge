a48dd7044f7aa2a15d00d7037349f82cc5fce839
Josua <jehret6@stud.hs-offenburg.de>
2025-05-11 09:56:31 +0200

    explain outputs

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
-from utils.model import response
 from utils.extract_json import get_json
 from utils.parse_pdf import parse
+from utils.json_to_xml import get_xml
+from utils.explain import highlight_words_in_pdf
 import os
 from pathlib import Path
+
+
 
 def main(leistungsverzeichnis: str):
     """
@@ -17,7 +20,9 @@ def main(leistungsverzeichnis: str):
 
     markdown = parse(leistungsverzeichnis, out_dir)
     jsons = get_json(markdown)
+    names = [jsons[i]["name"] for i in range(len(jsons))]
     xml = get_xml(jsons, markdown)
+    out_pdf = fr"data/output/{name}_highlighted.pdf"
+    highlight_words_in_pdf(leistungsverzeichnis, out_pdf, names)
 
-
-    return xml
+    return xml, out_pdf
